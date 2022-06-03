0234e23bfcdf66d37b877d4e9fc2e4ab61e1c5bd
Victor Hugo <victor.vivtorhugo@gmail.com>
2022-06-03 18:57:34 -0300

    Feat: Button Second Screen

---
+++ index.js
@@ -0,0 +1,12 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  document.getElementById("container").style.display = "none";
+  document.getElementById("containerLogin").style.display = "flex";
+});
+document.querySelector("formSuccess").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  document.getElementById("containerLogin").style.display = "none";
+  document.getElementById("container").style.display = "flex";
+});
